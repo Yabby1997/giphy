ea7a78b9addf845faaa47187b8d3e0f1e1eea1e7
Yabby1997 <yabby1997@gmail.com>
2022-09-13 21:03:08 +0900

    initial commit

---
+++ GiphySwiftUI/GiphySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphySwiftUIApp.swift
+//  GiphySwiftUI
+//
+//  Created by USER on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct GiphySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
